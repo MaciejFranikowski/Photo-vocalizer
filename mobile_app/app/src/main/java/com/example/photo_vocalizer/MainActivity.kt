d76e666995fffe2fd61c8ef57090cd41de2f2439
Maciej Franikowski <mfranikowski@gmail.com>
2022-11-18 20:01:47 +0100

    Import Model into app

--- mobile_app/app/src/main/java/com/example/photo_vocalizer/MainActivity.kt
+++ mobile_app/app/src/main/java/com/example/photo_vocalizer/MainActivity.kt
@@ -8,4 +8,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    /* Model usage
+    val model = FruitModel.newInstance(context)
+
+    // Creates inputs for reference.
+    val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 32, 32, 3), DataType.FLOAT32)
+    inputFeature0.loadBuffer(byteBuffer)
+
+    // Runs model inference and gets result.
+    val outputs = model.process(inputFeature0)
+    val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+    // Releases model resources if no longer used.
+    model.close()*/
+
 }
