3b4249ab46b85e64811856177bdd4d37369a897a
Maciej Franikowski <mfranikowski@gmail.com>
2022-11-27 17:43:07 +0100

    Add voice activated commands and colors

--- mobile_app/app/src/main/java/com/example/photo_vocalizer/MainActivity.kt
+++ mobile_app/app/src/main/java/com/example/photo_vocalizer/MainActivity.kt
@@ -20,6 +20,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.photo_vocalizer.ml.FruitModel
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
@@ -37,6 +38,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var image: Bitmap
     private lateinit var speechRecognizer : SpeechRecognizer
     private lateinit var speechRecognizerIntent : Intent
+    private lateinit var colors : Array<Int>
     private var isRecognizerSet : Boolean = false
     private var isImageSet : Boolean = false
     private val imageSize = 32
@@ -45,6 +47,7 @@ class MainActivity : AppCompatActivity() {
     private val cameraRequestPermissionCode = 101
     private val audioRequestPermissionCode = 102
     private val languageCode = "pl-PL"
+    private val classes = arrayOf("Apple", "Banana", "Orange")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -81,10 +84,36 @@ class MainActivity : AppCompatActivity() {
             override fun onResults(bundle: Bundle) {
                 val data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
                 Toast.makeText(localContext,data!![0], Toast.LENGTH_LONG).show()
+                if(data!![0].isNotEmpty())
+                    useRecognitionResult(data!![0])
             }
         })
         isRecognizerSet = true
         setOnTouchListener()
+    }
+
+    private fun useRecognitionResult(recognitionResult: String){
+
+        val regex1 = "wybierz".toRegex(RegexOption.IGNORE_CASE)
+        var match = regex1.find(recognitionResult)
+        if(match != null){
+            Toast.makeText(this, match.value, Toast.LENGTH_LONG).show()
+            pickFromGallery(null)
+        }
+
+        val regex2 = "zrób|wykonaj".toRegex(RegexOption.IGNORE_CASE)
+        match = regex2.find(recognitionResult)
+        if(match != null){
+            Toast.makeText(this, match.value, Toast.LENGTH_LONG).show()
+            takePhoto(null)
+        }
+
+        val regex3 = "klasyfik".toRegex(RegexOption.IGNORE_CASE)
+        match = regex3.find(recognitionResult)
+        if(match != null){
+            Toast.makeText(this, match.value, Toast.LENGTH_LONG).show()
+            classifyImage(null)
+        }
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -102,7 +131,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun takePhoto(view: View){
+    @Suppress("UNUSED_PARAMETER")
+    fun takePhoto(view: View?){
         if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(cameraIntent, cameraRequestCode)
@@ -111,7 +141,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun pickFromGallery(view: View){
+    @Suppress("UNUSED_PARAMETER")
+    fun pickFromGallery(view: View?){
         val cameraIntent =
             Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         startActivityForResult(cameraIntent, galleryRequestCode)
@@ -144,46 +175,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-     fun classifyImage(view: View) {
+    @Suppress("UNUSED_PARAMETER")
+     fun classifyImage(view: View?) {
          if(isImageSet){
              //iterate over each pixel and extract R, G, and B values. Add those values individually to the byte buffer.
              try {
                  val model = FruitModel.newInstance(applicationContext)
-
                  // Creates inputs for reference.
-                 val inputFeature0 =
-                     TensorBuffer.createFixedSize(intArrayOf(1, 32, 32, 3), DataType.FLOAT32)
-                 val byteBuffer: ByteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
-                 byteBuffer.order(ByteOrder.nativeOrder())
-                 val intValues = IntArray(imageSize * imageSize)
-                 image.getPixels(intValues, 0, image.width, 0, 0, image.width, image.height)
-                 var pixel = 0
-                 for (i in 0 until imageSize) {
-                     for (j in 0 until imageSize) {
-                         val `val` = intValues[pixel++] // RGB
-                         byteBuffer.putFloat((`val` shr 16 and 0xFF) * (1f / 1))
-                         byteBuffer.putFloat((`val` shr 8 and 0xFF) * (1f / 1))
-                         byteBuffer.putFloat((`val` and 0xFF) * (1f / 1))
-                     }
-                 }
-                 inputFeature0.loadBuffer(byteBuffer)
-
+                 val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 32, 32, 3), DataType.FLOAT32)
+                 inputFeature0.loadBuffer(createByteBuffer())
                  // Runs model inference and gets result.
                  val outputs: FruitModel.Outputs = model.process(inputFeature0)
                  val outputFeature0: TensorBuffer = outputs.outputFeature0AsTensorBuffer
                  val confidences = outputFeature0.floatArray
-                 // find the index of the class with the biggest confidence.
-                 var maxPos = 0
-                 var maxConfidence = 0f
-                 for (i in confidences.indices) {
-                     if (confidences[i] > maxConfidence) {
-                         maxConfidence = confidences[i]
-                         maxPos = i
-                     }
-                 }
-                 val classes = arrayOf("Apple", "Banana", "Orange")
-                 resultText.text = classes[maxPos]
-
+                 // set the found class based on the confidences
+                 setClassifiedClass(confidences)
                  // Releases model resources if no longer used.
                  model.close()
              } catch (e: Exception) {
@@ -195,12 +201,47 @@ class MainActivity : AppCompatActivity() {
          }
     }
 
+    private fun createByteBuffer() : ByteBuffer{
+        // (Float[4] * imageSize ^2 * rgbValues[3])
+        val byteBuffer: ByteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
+        byteBuffer.order(ByteOrder.nativeOrder())
+        val intValues = IntArray(imageSize * imageSize)
+        image.getPixels(intValues, 0, image.width, 0, 0, image.width, image.height)
+        var pixel = 0
+        // Iterate over all the pixels, getting the RGB values for each one
+        for (i in 0 until imageSize) {
+            for (j in 0 until imageSize) {
+                val `val` = intValues[pixel++] // RGB
+                byteBuffer.putFloat((`val` shr 16 and 0xFF) * (1f / 1))
+                byteBuffer.putFloat((`val` shr 8 and 0xFF) * (1f / 1))
+                byteBuffer.putFloat((`val` and 0xFF) * (1f / 1))
+            }
+        }
+        return byteBuffer
+    }
+
+    private fun setClassifiedClass(confidences: FloatArray){
+        var maxPos = 0
+        var maxConfidence = 0f
+        for (i in confidences.indices) {
+            if (confidences[i] > maxConfidence) {
+                maxConfidence = confidences[i]
+                maxPos = i
+            }
+        }
+        resultText.text = classes[maxPos]
+        resultText.setTextColor(colors[maxPos])
+    }
+
     private fun assignReferences(){
         photoButton = findViewById(R.id.photoButton)
         galleryButton = findViewById(R.id.galleryButton)
         imageView = findViewById(R.id.imageView)
         resultText = findViewById(R.id.resultTextView)
         listenButton = findViewById(R.id.listenButton)
+        colors = arrayOf(ContextCompat.getColor(this, R.color.red),
+            ContextCompat.getColor(this, R.color.yellow),
+            ContextCompat.getColor(this, R.color.orange))
     }
 
     override fun onRequestPermissionsResult(
